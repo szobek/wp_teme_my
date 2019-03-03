70cef2ec5a2c8b119401959ff23d22895c8e8fd7
Kunszt Norbert <kunszt.norbert@gmail.com>
2019-03-03 11:03:48 +0100

    add fullwidth page template

---
+++ fullwidth.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Template Name: teljes
+ */
+get_header();
+
+?>
+
+
+
+
+<?php if(have_posts()) : while(have_posts()) : the_post()  ?>
+
+    <?php the_content() ?>
+
+<?php endwhile; endif; ?>
+
+
+<?php get_footer();?>
